08ed9590b1dda913fbb2f8550717b8643cdf48b1
LongoMichele <michele.longo@student.unife.it>
2019-09-14 17:15:51 +0200

    Code cleanup part 4

--- website/index.php
+++ website/index.php
@@ -12,24 +12,22 @@
 
 		<title>TATE Museum | Search</title>
 	</head>
-	<body style="font-family: Arial; font-size: 125%; color: #444444;">
+	<body>
 		<nav class="level" style="margin-bottom: 0; border-bottom: solid #bbb 5px;">
 			<div class="level-left">
 				<div class="level-item">
-				<p class="subtitle is-3">
-					<strong>TATE</strong> Museum
-				</p>
+					<p class="subtitle is-3">
+						<strong>TATE</strong> Museum
+					</p>
 				</div>
 			</div>
-
 			<div class="level-right">
 				<p class="level-item"><strong>Home</strong></p>
 				<p class="level-item"><a href="https://tate.org.uk">Tate Official</a></p>
-				<p class="level-item">
-					<a href="https://bulma.io"><img src="res/made-with-bulma.png" alt="Bulma" width=128 height=30></a>
-				</p>
+				<p class="level-item"><a href="https://bulma.io"><img src="res/made-with-bulma.png" alt="Bulma" width=128 height=30></a></p>
 			</div>
 		</nav>
+
 		<?php
 			$server = "localhost";
 			$user	= "michele";
@@ -44,45 +42,39 @@
 
 				if($link->connect_error) {
 					echo 'Unable to connect to ' .$db. ' DB<br>';
-					echo 'Error code: ' . $link->connect_error . '<br>';
+					echo 'Error code: ' .$link->connect_error. '<br>';
 					exit;
 				}
 			}
 
-			$id = $_GET["artistID"];
+			$id				= $_GET["artistID"];
 
 			$general 		= $_GET["general"];
 
 			$infos			= array(isset($_GET["artistInfo"]) ? 1 : 0, isset($_GET["artworkInfo"]) ? 1 : 0);
-
 			$artist_name	= $_GET["name"];
 			$places			= $_GET["places"];
 			$artist_year	= strval($_GET["artistYear"]);
 			$gender			= $_GET["gender"];
-
 			$title			= $_GET["title"];
 			$inscription	= $_GET["inscription"];
 			$medium			= $_GET["medium"];
 			$artwork_year	= strval($_GET["artworkYear"]);
 			$artist_role	= $_GET["artistRole"];
 
-			// options:
 			$order			= $_GET["order"];
-			
-			if($id != null) {
-				$fields1 = array('Title', 'Year', 'Medium', 'Inscription', 'ArtistRole', 'Name');
+
+			if($id != null) { // Lista delle opere di un dato artista
+				$fields = array('Title', 'Year', 'Medium', 'Inscription', 'ArtistRole', 'Name');
 				$query = '
-					SELECT DISTINCT Title, Year, Medium, Inscription, ArtistRole, Name, Artwork.ThumbnailUrl, Artwork.ID ID, Artist.ID IDA
+					SELECT DISTINCT Title, Year, Medium, Inscription, ArtistRole, Name, ThumbnailUrl, Artwork.ID ID, Artist.ID IDA
 					FROM Artist JOIN Artwork ON Artist.ID = Artwork.ArtistId
 					WHERE Artist.ID = ' .$id. '
 					ORDER BY Title '.$order.'
 				;';
-				
-				$query_count = 1;
-				$fields = array($fields1);
 			}
 			else {
-				if($general != '' and !$infos[0] and !$infos[1]) { // ricerca libera
+				if($general != '' and !$infos[0] and !$infos[1]) { // Ricerca Generale
 					$fieldsArtist = array('Name', 'Gender', 'PlaceOfBirth', 'PlaceOfDeath', 'YearOfBirth', 'YearOfDeath');
 					$bestArtist = null;
 					for($i = 0; $i < count($fieldsArtist); $i++) {
@@ -104,11 +96,11 @@
 					$bestArtwork = null;
 					for($i = 0; $i < count($fieldsArtwork); $i++) {
 						$option = '
-							SELECT DISTINCT Title, Year, Medium, Inscription, ArtistRole, Artist.Name Name, Artist.ID IDA, Artwork.ID ID
-							FROM Artwork JOIN Artist ON Artwork.ArtistId=Artist.ID
-							WHERE ' .$fieldsArtwork[$i]. ' LIKE "%'.$general.'%"
-							ORDER BY ' .$fieldsArtwork[$i]. ' ' .$order. '
-							LIMIT 200
+						SELECT DISTINCT Title, Year, Medium, Inscription, ArtistRole, Artist.Name Name, Artist.ID IDA, Artwork.ID ID
+						FROM Artwork JOIN Artist ON Artwork.ArtistId=Artist.ID
+						WHERE ' .$fieldsArtwork[$i]. ' LIKE "%'.$general.'%"
+						ORDER BY ' .$fieldsArtwork[$i]. ' ' .$order. '
+						LIMIT 200
 						;';
 						$result = $link->query($option);
 						if($bestArtwork == null or ($bestArtwork > $result->num_rows and $result->num_rows > 0)) {
@@ -138,27 +130,23 @@
 					$query_count = 1; 
 				}
 				else {
-					if($general == '' and !$infos[0] and !$infos[1]) { // vuoto
-						$fields1 = array('Name', 'Gender', 'PlaceOfBirth', 'PlaceOfDeath', 'YearOfBirth', 'YearOfDeath');
+					if($general == '' and !$infos[0] and !$infos[1]) { // Ricerca vuota
+						$fields = array('Name', 'Gender', 'PlaceOfBirth', 'PlaceOfDeath', 'YearOfBirth', 'YearOfDeath');
 						$query ='
 							SELECT DISTINCT Name, Gender, PlaceOfBirth, PlaceOfDeath, YearOfBirth, YearOfDeath, Artist.ID IDA
 							FROM Artist JOIN Artwork ON Artist.ID=Artwork.ArtistId
 							ORDER BY Name '.$order.'
 							LIMIT 200
-							;	
-						';
-
-						$query_count = 1;
-						$fields = array($fields1);
+						;';
 					}
 					else {
-						if($infos[0]) {	// ricerca gudata, artista
+						if($infos[0]) {	// Ricerca gudata -> Artist
 							if($artist_name == '') $artist_name = '%';
 							if($places == '') $places = '%';
 							if($artist_year == '') $artist_year = '%';
 							if($gender == 'all') $gender = '%';
 
-							$fields1 = array('Name', 'Gender', 'YearOfBirth', 'YearOfDeath', 'PlaceOfBirth', 'PlaceOfDeath');
+							$fields = array('Name', 'Gender', 'YearOfBirth', 'YearOfDeath', 'PlaceOfBirth', 'PlaceOfDeath');
 							$query = '
 								SELECT DISTINCT Name, Gender, YearOfBirth, YearOfDeath, PlaceOfBirth, PlaceOfDeath, Artist.ID IDA
 								FROM Artist
@@ -168,19 +156,15 @@
 								AND (YearOfBirth LIKE "'.$artist_year.'" OR YearOfDeath LIKE "'.$artist_year.'")
 								ORDER BY Name '.$order.'
 							;';
-							
-							$query_count = 1;
-							$fields = array($fields1);
-
 						}
-						if($infos[1]) {  // ricerca guidata, artwork
+						if($infos[1]) {  // Ricerca gudata -> Artwork
 							if($title == '') $title = '%';
 							if($inscription == '') $inscription = '%';
 							if($medium == '') $medium = '%';
 							if($artwork_year == '') $artwork_year = '%';
 							if($artist_role == 'all') $artist_role = '%';
 
-							$fields1 = array('Title', 'Year', 'Medium', 'Inscription', 'ArtistRole', 'Name');
+							$fields = array('Title', 'Year', 'Medium', 'Inscription', 'ArtistRole', 'Name');
 							$query = '
 								SELECT DISTINCT Title, Year, Medium, Inscription, ArtistRole, Artist.Name Name, Artwork.ThumbnailUrl, Artwork.ID ID, Artist.ID IDA
 								FROM Artist JOIN Artwork ON Artist.ID = Artwork.ArtistId
@@ -192,24 +176,19 @@
 								ORDER BY Title '.$order.'
 								LIMIT 200
 							;';
-							
-							$query_count = 1;
-							$fields = array($fields1);
-
 						}
-						if($infos[0] and $infos[1]) {  // ricerca guidata, artista + artwork
+						if($infos[0] and $infos[1]) {  // Ricerca gudata -> Artist + Artwork
 							if($artist_name == '') $artist_name = '%';
 							if($places == '') $places = '%';
 							if($artist_year == '') $artist_year = '%';
+							if($gender == 'all') $gender = '%';
 
 							if($title == '') $title = '%';
 							if($inscription == '') $inscription = '%';
 							if($medium == '') $medium = '%';
 							if($artwork_year == '') $artwork_year = '%';
 
-							if($gender == 'all') $gender = '%';
-
-							$fields1 = array('Title', 'Year', 'Medium', 'Inscription', 'ArtistRole', 'Name', 'Gender', 'YearOfBirth', 'YearOfDeath', 'PlaceOfBirth', 'PlaceOfDeath');
+							$fields = array('Title', 'Year', 'Medium', 'Inscription', 'ArtistRole', 'Name', 'Gender', 'YearOfBirth', 'YearOfDeath', 'PlaceOfBirth', 'PlaceOfDeath');
 							$query = '
 								SELECT DISTINCT Title, Year, Medium, Inscription, ArtistRole, Artist.Name Name, Gender, YearOfBirth, YearOfDeath, PlaceOfBirth, PlaceOfDeath, Artwork.ThumbnailUrl, Artwork.ID ID, Artist.ID IDA
 								FROM Artist JOIN Artwork ON Artist.ID = Artwork.ArtistId
@@ -225,9 +204,6 @@
 								ORDER BY Title '.$order.'
 								LIMIT 200
 							;';
-							
-							$query_count = 1;
-							$fields = array($fields1);
 						}
 					}
 				}
@@ -241,14 +217,14 @@
 				<div class="tabs is-toggle is-centered">
 					<ul>
 						<li class="is-active" id="2">
-							<a href="javascript:SearchTabs( 'guided', '2') ">
-								<span class="icon is-small "><i class="fas "></i></span>
+							<a href="javascript:SearchTabs('guided', '2')">
+								<span class="icon is-small"><i class="fas"></i></span>
 								<span>Guided</span>
 							</a>
 						</li>
 						<li class="" id="1">
-							<a href="javascript:SearchTabs( 'general', '1') ">
-								<span class="icon is-small "><i class="fas "></i></span>
+							<a href="javascript:SearchTabs('general', '1')">
+								<span class="icon is-small"><i class="fas"></i></span>
 								<span>General</span>
 							</a>
 						</li>
@@ -258,7 +234,7 @@
 				<div id="SearchTabs">
 					<div id="guided" style="display: block;">
 						<b>Filters</b><br>
-						<div style="text-align: left;"><center>
+						<div style="text-align: left;">
 							<input type="checkbox" id="ArtistInfo" name="artistInfo" value="true"> Artist
 							<div id="ArtistDiv" style="margin-bottom: -5%;" hidden>
 								<div class="control">
@@ -269,11 +245,11 @@
 										<label class="label">Gender</label>
 										<div class="control">
 											<div class="select">
-											<select name="gender">
-												<option value="all">All</option>
-												<option value="male">Male</option>
-												<option value="female">Female</option>
-											</select>
+												<select name="gender">
+													<option value="all">All</option>
+													<option value="male">Male</option>
+													<option value="female">Female</option>
+												</select>
 											</div>
 										</div>
 									</div>
@@ -291,23 +267,25 @@
 										<label class="label">Artist role<br></label>
 										<div class="control">
 											<div class="select">
-											<select name="artistRole">
-												<option value="all">All</option>
-												<?php
-													$roleHelper = 'SELECT DISTINCT ArtistRole FROM Artwork;';
-													$result = $link->query($roleHelper);
-													if($result->num_rows > 0) {
-														while($row = $result->fetch_assoc()) 
-															echo '<option value="'.$row["ArtistRole"].'">'.$row["ArtistRole"].'</option>';
-													}
-												?>
-											</select>
+												<select name="artistRole">
+													<option value="all">All</option>
+													<?php
+														$roleHelper = '
+															SELECT DISTINCT ArtistRole
+															FROM Artwork
+														;';
+														$result = $link->query($roleHelper);
+														if($result->num_rows > 0) {
+															while($row = $result->fetch_assoc()) 
+																echo '<option value="'.$row["ArtistRole"].'">'.$row["ArtistRole"].'</option>';
+														}
+													?>
+												</select>
 											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-						<center>
 						</div>
 					</div>
 
@@ -323,84 +301,82 @@
 						<label class="label">Order by<br></label>
 						<div class="control">
 							<div class="select">
-							<select name="order">
-								<option value="asc">A - Z</option>
-								<option value="desc">Z - A</option>
-							</select>
+								<select name="order">
+									<option value="asc">A - Z</option>
+									<option value="desc">Z - A</option>
+								</select>
 							</div>
 						</div>
 					</div>
 				</div>
-
 				<br>
-				<button class="button is-primary is-rounded is-medium" type="submit" style="margin-bottom: 5%;">Search</button>
+				<button class="button is-primary is-rounded is-medium" type="submit">Search</button>
 			</form>
 		</div>
 
 		<div class="split right">
 			<br>
 			<?php
-				for($j = 0; $j < $query_count; $j++) {
-					$result = $link->query($query);
-					echo '<h3 class="title is-3" style="text-align: center;">Search Results (Total: ' .$result->num_rows. ')</h3>';
-					echo '<div id="tableContainer" class="tableContainer">';
-					if($result->num_rows >= 15) {
-						echo '<table cellpadding="0" cellspacing="0" width="100%" class="scrollTable"><thead class="fixedHeader"><tr>';
-						for($i = 0; $i<count($fields[$j]); $i++) {
-							echo '<th width="' .(100/count($fields[$j])). '%"><b>' .$fields[$j][$i]. '</b></th>';
-						}
-						echo '</tr></thead><tbody class="scrollContent">';
+				$result = $link->query($query);
+				echo '<h3 class="title is-3" style="text-align: center;">Search Results (Total: ' .$result->num_rows. ')</h3>';
+				echo '<div id="tableContainer" class="tableContainer">';
+				if($result->num_rows >= 15) {
+					echo '<table cellpadding="0" cellspacing="0" width="100%" class="scrollTable"><thead class="fixedHeader"><tr>';
+					for($i = 0; $i<count($fields); $i++) {
+						echo '<th width="' .(100/count($fields)). '%"><b>' .$fields[$i]. '</b></th>';
+					}
+					echo '</tr></thead><tbody class="scrollContent">';
 
-						for($k = 0; $row = $result->fetch_assoc(); $k++) {
-							if($k % 2 != 0) {
-								echo '<tr class="alternateRow">';
+					for($k = 0; $row = $result->fetch_assoc(); $k++) {
+						if($k % 2 != 0) {
+							echo '<tr class="alternateRow">';
+						}
+						else {
+							echo '<tr>';
+						}
+
+						for($i = 0; $i<count($fields); $i++) {
+							if($fields[$i] == "Title") {
+								echo '<td width="' .(100/count($fields)). '%"><a href="artwork.php?id=' .$row["ID"]. '">' .$row[$fields[$i]]. '</a></td>';
 							}
 							else {
-								echo '<tr>';
-							}
-							
-							for($i = 0; $i<count($fields[$j]); $i++) {
-								if($fields[$j][$i] == "Title") {
-									echo '<td width="' .(100/count($fields[$j])). '%"><a href="artwork.php?id=' .$row["ID"]. '">' .$row[$fields[$j][$i]]. '</a></td>';
+								if($fields[$i] == "Name") {
+									echo '<td width="' .(100/count($fields)). '%"><a href="artist.php?id=' .$row["IDA"]. '">' .$row[$fields[$i]]. '</a></td>';
 								}
 								else {
-									if($fields[$j][$i] == "Name") {
-										echo '<td width="' .(100/count($fields[$j])). '%"><a href="artist.php?id=' .$row["IDA"]. '">' .$row[$fields[$j][$i]]. '</a></td>';
-									}
-									else {
-										echo '<td width="' .(100/count($fields[$j])). '%">' .$row[$fields[$j][$i]]. '</td>';
-									}
+									echo '<td width="' .(100/count($fields)). '%">' .$row[$fields[$i]]. '</td>';
 								}
 							}
-							echo '</tr>';
 						}
-						echo '</tbody></table></div>';
+						echo '</tr>';
 					}
-					else {
-						if($result->num_rows > 0){
-							echo '<table cellpadding="0" cellspacing="0" width="100%"><thead><tr>';
-						for($i = 0; $i<count($fields[$j]); $i++) {
-							echo '<th width="' .(100/count($fields[$j])). '%" style="text-align: center;"><b>' .$fields[$j][$i]. '</b></th>';
+					echo '</tbody></table></div>';
+				}
+				else {
+					if($result->num_rows > 0){
+						echo '<table cellpadding="0" cellspacing="0" width="100%"><thead><tr>';
+						for($i = 0; $i<count($fields); $i++) {
+							echo '<th width="' .(100/count($fields)). '%" style="text-align: center;"><b>' .$fields[$i]. '</b></th>';
 						}
 						echo '</tr></thead><tbody>';
 
 						for($k = 0; $row = $result->fetch_assoc(); $k++) {
 							echo '<tr>';
-							for($i = 0; $i<count($fields[$j]); $i++) {
+							for($i = 0; $i<count($fields); $i++) {
 								$class = "";
 								if($k%2 != 0) {
 									$class = "alternate";
 								}
-								echo '<td class="' .$class. '" width="' .(100/count($fields[$j])). '%">';
-								if($fields[$j][$i] == "Title") {
-									echo '<a href="artwork.php?id=' .$row["ID"]. '">' .$row[$fields[$j][$i]]. '</a>';
+								echo '<td class="' .$class. '" width="' .(100/count($fields)). '%">';
+								if($fields[$i] == "Title") {
+									echo '<a href="artwork.php?id=' .$row["ID"]. '">' .$row[$fields[$i]]. '</a>';
 								}
 								else {
-									if($fields[$j][$i] == "Name") {
-										echo '<a href="artist.php?id=' .$row["IDA"]. '">' .$row[$fields[$j][$i]]. '</a>';
+									if($fields[$i] == "Name") {
+										echo '<a href="artist.php?id=' .$row["IDA"]. '">' .$row[$fields[$i]]. '</a>';
 									}
 									else {
-										echo $row[$fields[$j][$i]];
+										echo $row[$fields[$i]];
 									}
 								}
 								echo '</td>';
@@ -408,8 +384,9 @@
 							echo '</tr>';
 						}
 						echo '</tbody></table></div>';
-						}
-						else echo 'Internal Error OR Empty Result<br><br>' .$query;	// potremmo scrivere qualcosaltro...
+					}
+					else {
+						echo 'The informations recived from the input form lead to an empty result.';
 					}
 				}
 				$link->close();
